a1e8eecdc43900535be9d5aa202bfc34c92ac648
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2024-06-19 18:31:53 +0530

    monor

--- ClientApp/src/app/Common/carousel/carousel.component.ts
+++ ClientApp/src/app/Common/carousel/carousel.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
-  myInterval = 10000;
-  activeSlideIndex = 0;
-  slides: { image: string; text?: string }[] = [
-    { image: 'assets/Images/Carousel3.jpg', text: 'Beautiful nature image 3' },
-    { image: 'assets/Images/Carousel4.jpg', text: 'Beautiful nature image 2' },
-    { image: 'assets/Images/Carousel5.jpg', text: 'Beautiful nature image 1' }
-  ];
 
-  slideChangeMessage = '';
-  log(event: number) {
-    // simple hack for expression has been changed error
-    setTimeout(() => {
-      this.slideChangeMessage = `Slide has been switched: ${event}`;
-    });
-  }
 }
